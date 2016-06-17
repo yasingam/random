e8958af55fbf0d931d2cf1bdc5b6a34919a61d21
Yasin Gamieldien <yasingamieldien@gmail.com>
2016-06-17 18:17:10 +0200

    Update stlread.py

--- stlread.py
+++ stlread.py
@@ -1,30 +1,87 @@
 #Yasin Gamieldien
 #Python 2/3 compatible
-#Requires Numpy
+#Requires Numpy, Pandas
 #Free to use, alter, and distribute
 
 import numpy as np
+import pandas as pd
 
-def stl(path):
+def stl_read(path):
+
     """
-    >>> faces, normals, vertices = stl(path)
+    Return faces, normals, and vertex co-ordinates of a binary STL
 
-    uses numpy to extract information from STL files
+    >>> faces, normals, x1, y1, z1, x2, y2, z2, x3, y3, z3 = stl_read(path)
     """
 
-    data_raw = np.fromfile(path, dtype='uint8')
-    stl_header = data_raw[:80]
-    faces = np.frombuffer(data_raw[80:84].tobytes(), dtype='uint32')[0]
+    raw_data = np.fromfile(path, dtype='uint8')
 
-    stl_raw = data_raw[84:]
-    stl_raw_uint16 = np.frombuffer((stl_raw.tobytes()), dtype='uint16')
-    stl_attr_index = np.arange(24, len(stl_raw_uint16), 25)
-    stl_uint16 = np.delete(stl_raw_uint16, stl_attr_index)
-    stl = np.frombuffer(stl_uint16.tobytes(), dtype='float32')
-    stl_shaped = np.reshape(stl, (-1, 3))
-    normals = stl_shaped[0:-1:4]
+    header = raw_data[:80]
 
-    normals_index = np.arange(0, len(stl_shaped), 4)
-    vertices = np.delete(stl_shaped, normals_index).reshape(-1, 3)
+    faces = np.frombuffer(raw_data[80:84].tobytes(), dtype='uint32')[0]
 
-    return faces, normals, vertices
+    raw_stl = np.frombuffer(raw_data[84:].tobytes(), dtype='uint16')
+    raw_stl = np.ravel(np.reshape(raw_stl, (-1, 25))[:,:24])
+    raw_stl = np.frombuffer(raw_stl.tobytes(), dtype='float32')
+
+    stl = np.reshape(raw_stl, (-1, 12))
+
+    normals = stl[:, :3]
+
+    x1 = stl[:, 3]
+    y1 = stl[:, 4]
+    z1 = stl[:, 5]
+
+    x2 = stl[:, 6]
+    y2 = stl[:, 7]
+    z2 = stl[:, 8]
+
+    x3 = stl[:, 9]
+    y3 = stl[:, 10]
+    z3 = stl[:, 11]
+
+    return faces, normals, x1, y1, z1, x2, y2, z2, x3, y3, z3
+
+
+def stl_xyz(path):
+
+    """
+    Return x, y, z co-ordinates of vertices
+
+    >>> x, y, z = stl_xyz(path)
+    """
+
+    faces, normals, x1, y1, z1, x2, y2, z2, x3, y3, z3 = stl_read(path)
+
+    x = np.append(x1, x2)
+    x = np.append(x, x3)
+
+    y = np.append(y1, y2)
+    y = np.append(y, y3)
+
+    z = np.append(z1, z2)
+    z = np.append(z, z3)
+
+    return x, y, z
+
+
+def unique_vertices(path):
+
+    """
+    Return unique vertices
+    (Computationally heavy process)
+
+    >>> x, y, z = unique_vertices(path)
+    """
+    x, y, z = stl_xyz(path)
+
+    stl_data = np.column_stack((xi, yi, zi))
+    stl_pandas = pd.DataFrame(stl_data)
+
+    stl_unique = np.array(stl_pandas.drop_duplicates())
+
+    x = stl_unique[:, 0]
+    y = stl_unique[:, 1]
+    z = stl_unique[:, 2]
+
+    return x, y, z

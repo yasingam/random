5b32e867fa911bba6412e721b0d3fb770fb67d57
Yasin Gamieldien <yasingamieldien@gmail.com>
2017-06-18 14:18:27 +0200

    Update gradient_descent.m

--- gradient_descent.m
+++ gradient_descent.m
@@ -1,6 +1,7 @@
 #gradient_descent.m
 #Gradient descent for Octave
-
-function [theta] = gradientDescent(X, y, alpha)
+#theta: column vector, X: matrix, alpha: real, Y: column vector
+function [theta] = gradientDescent(X, y, alpha) #returns theta, a column vector
 
 theta = theta - (alpha*(1/m)*sum(X.*(X*theta-y)))'
+#result of sum(X.*(X*theta-y)) is a row vector of 1 row and n columns (where n = number of features)
